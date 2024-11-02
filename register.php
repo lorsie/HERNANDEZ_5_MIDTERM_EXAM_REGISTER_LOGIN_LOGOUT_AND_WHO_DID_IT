b1e34962a089a19192e622e936b45f118aa5ac01
lorsie <152486611+lorsie@users.noreply.github.com>
2024-11-03 01:53:23 +0800

    Create register.php

---
+++ register.php
@@ -0,0 +1,45 @@
+<?php  
+require_once 'core/models.php'; 
+require_once 'core/handleForms.php'; 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Register Now!</title>
+	<style>
+    body {
+        font-family: "system-ui";
+        background-color: #FFCCEA
+    }
+    input {
+        font-size: 1.5em;
+        height: 40px;
+        width: 200px;
+        background-color: #D4BDAC
+    }
+    put [type = "submit"]{
+        font-weight:bold;
+    }
+   </style> 
+	
+</head>
+<body>
+	<h1>Register here!</h1>
+	<?php if (isset($_SESSION['message'])) { ?>
+		<h1 style="color: red;"><?php echo $_SESSION['message']; ?></h1>
+	<?php } unset($_SESSION['message']); ?>
+	<form action="core/handleForms.php" method="POST">
+		<p>
+			<label for="username">Username</label>
+			<input type="text" name="username">
+		</p>
+		<p>
+			<label for="username">Password</label>
+			<input type="password" name="password">
+			<input type="submit" name="registerUserBtn">
+		</p>
+	</form>
+</body>
+</html>
